927bdf2e8083010c30fbb47e64975d091070a91d
Almosk <alvin.kita@outlook.fr>
2024-07-18 12:06:11 +0200

    Init: package com.tp.mybestyoutube.database

--- app/src/main/java/com/tp/mybestyoutube/database/AppDatabase.java
+++ app/src/main/java/com/tp/mybestyoutube/database/AppDatabase.java
@@ -1,2 +1,27 @@
-package com.tp.mybestyoutube.database;public class AppDatabase {
+package com.tp.mybestyoutube.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.tp.mybestyoutube.database.dao.YoutubeVideoDao;
+import com.tp.mybestyoutube.database.entity.YoutubeVideo;
+
+/**
+ * classe permettant de créer la base de données et d’accéder au DAO
+ * afin de stocker les informations d'une vidéo Youtube.
+ */
+@Database(entities = {YoutubeVideo.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "My_Best_Youtube";
+
+    public static AppDatabase getDb(Context context) {
+        return Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME)
+                .allowMainThreadQueries().build();
+    }
+
+    public abstract YoutubeVideoDao youtubeVideoDao();
 }
